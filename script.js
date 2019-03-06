7520cc101144c4a079c33e1e212dbc1a2bb0b688
PaulR282 <paul.rost282@gmail.com>
2019-03-06 14:19:59 +0100

    last

--- script.js
+++ script.js
@@ -8,4 +8,14 @@ function display_toggle(container) {
 	}
 	
 }
+function leange() {
+	var n1 = document.getElementById('input1').value;
+	var oper = document.getElementById('exampleFormControlSelect1').value;
+	if (oper==="mm")
+		var n1 = document.getElementById('mm').value = n1;
+		var n1 = document.getElementById('cm').value = n1/10;
+		var n1 = document.getElementById('m').value = n1/100;
+		var n1 = document.getElementById('km').value = n1/1000;
+
+}
 
