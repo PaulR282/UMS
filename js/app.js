7b0898870ef50ae72653de2df1a541f1335ea856
PaulR282 <paul.rost282@gmail.com>
2019-04-10 14:33:01 +0200

    last

---
+++ js/app.js
@@ -0,0 +1,50 @@
+angular.module('ngUMS', ['ui.bootstrap']);
+var app = angular.module("ngUMS", []);
+app.controller("ngCtrl", function($scope) {
+	$scope.laenge=[
+"μm","mm","cm","dm","m","km","Yards","Zoll","Meilen","Seemeilen","Fuß",]
+
+$scope.volumen=[
+"ml","dl","l","mm³","cm³","dm³","m³","Gallonen(US)","Zoll³","Fuß³","Yard³",]
+$scope.volumen2=[
+"mm","cm","dm","m","Zoll","Fuß","Yard",]
+$scope.masse=[
+"kg","g","mg","t","Pfund(lb)","Unze(oz)",]
+
+$scope.flaeche=[
+"mm","cm","dm","m","km","Zoll","Fuß","Yard","Meile",]
+$scope.flaeche2=[
+"mm²","cm²","dm²","m²","ar","ha","km²","Zoll²","Fuß²","Yard²","Meile²",]
+
+$scope.zeit=[
+"ns","ms","s","min","Stunden","Tage","Wochen","Jahre",]
+
+$scope.winkel=[
+"Grad","Radiant",]
+
+$scope.temperatur=[
+"°C","K","°F",]
+
+$scope.geschwindigkeit=[
+"m/s","Fuß/s","km/h","Meilen/h","Knoten",]
+
+$scope.druck=[
+"Pa","Bar","Torr","PhA","lbf/in²",]
+$scope.kraft=[
+"N","kN", "kgf", "lbf"]
+
+$scope.arbeit=[
+"J","kJ","cal","kcal","Wh","kWh",]
+
+$scope.leistung=[
+"W","kW","mW","kcal/s","kcal/h","PS"]
+
+$scope.kochen=[
+"ml(cc)","teaspoon(GB)","teaspoon(US)","cup(US)","cup(GB)","Unzen(US)","Unzen(GB)",]
+
+$scope.daten=[
+"b","kb","mb","gb","tb"]
+$scope.waehrung=[
+"€", "$"]
+});
+
--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+
 
 var open = [];
 var pi = Math.PI;
@@ -594,7 +594,6 @@ function kochen2() {
 }
 // done ^
 
-   
 
 function waehrung2() {
     m50 = m50 + 1;
@@ -603,11 +602,10 @@ function waehrung2() {
         /*var euriusd = document.getElementsByClassName('hiddenOne')[0].id;
         alert(euriusd)
         var test = euriusd[0].value;
-        console.log(test, euriusd)
-        var test = &('.left:first');
-        test2 =test.attr('main-value');
-        console.log(test2);*/
-        console.info("left:first: " $(main-value).attr('left:first'));
+        console.log(test, euriusd)*/
+        const test = document.querySelectorAll('.left');
+        test2 =test.getAttribute('main-value');
+        alert(test2);
         var m1 = document.getElementById('input17').value;
         var m6 = m1.replace(/,/, ".");
         if (isNaN(m6)) {
@@ -631,4 +629,3 @@ function waehrung2() {
 function test() {
 
 }
-});
